fc693a56ea21b5e2bbeb074ba0b600f02df64693
dave-ai <omotoyedave4@gmail.com>
2025-12-15 05:06:15 +0100

    schema

--- model/Product.js
+++ model/Product.js
@@ -6,6 +6,7 @@ const productSchema = mongoose.Schema({
     price: { type: Number, required: true },
     stock: { type: Number, default: 0 },
     category: { type: String, required: true },
+      originalPrice: { type: Number },
     rating: { type: Number, default: 0 },
     numReviews: { type: Number, default: 0 },
     reviews: [{ rating: Number, comment: String, createdAt: Date }],
