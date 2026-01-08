0e5d69ccbae73232929398e747611b5f34afcb2d
davex-ai <omotoyedave4@gmail.com>
2026-01-08 02:25:40 +0100

    render aint rendering

--- model/Wishlist.js
+++ model/Wishlist.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const wishlistSchema = mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
