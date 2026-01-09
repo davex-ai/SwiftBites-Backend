ea21635923b69ce70f17305396e48a6199a45a60
davex-ai <omotoyedave4@gmail.com>
2026-01-09 12:25:34 +0100

    ...

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -26,3 +26,11 @@ export const protect = async (req, res, next) => {
         return res.status(401).json({ message: "Not authorized, no token" });
     }
 };
+
+export const admin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).json({ message: "Not authorized as an admin" });
+  }
+};
